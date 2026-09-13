e9c648661362fd4584ae05946ca2e72565c8d7f3
montasir132 <montasiralam132@gmail.com>
2026-09-13 17:14:43 +0600

    Technology json and type added

---
+++ src/components/types/technologyType.ts
@@ -0,0 +1,10 @@
+export interface ITechnology {
+    id: string;
+    name: string;
+    logo: string;
+    badge?: string;
+    category: string;
+    level: string;
+    rating: number;
+    description: string;
+}
