f1e6548f9ab5284eb46a75009e5c79680bbfab22
montasir132 <montasiralam132@gmail.com>
2026-09-13 22:44:23 +0600

    ToastContainer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
-import { ToastContainer } from 'react-toastify'
-import Navbar from './components/Navbar/navbar'
-import Banner from './components/Hero/banner'
-import Footer from './components/Footer/footer'
-import type { ITechnology } from './components/types/technologyType'
-import { Suspense, useState } from 'react'
-import Technologies from './components/Technologies/Technologies'
+import { Bounce, ToastContainer } from "react-toastify";
+import Navbar from "./components/Navbar/navbar";
+import Banner from "./components/Hero/banner";
+import Footer from "./components/Footer/footer";
+import type { ITechnology } from "./components/types/technologyType";
+import { Suspense, useState } from "react";
+import Technologies from "./components/Technologies/Technologies";
 
-const technologiesFetch = async () : Promise<ITechnology[]> => {
-  const res = await fetch("technologies.json")
-  const data = await res.json()
+const technologiesFetch = async (): Promise<ITechnology[]> => {
+  const res = await fetch("technologies.json");
+  const data = await res.json();
   return data;
-}
+};
 
 function App() {
-const [technologiesPromise] = useState(() => technologiesFetch())
+  const [technologiesPromise] = useState(() => technologiesFetch());
 
   return (
     <div className="container mx-auto font-jakarta">
-      <Navbar/>
-      <Banner/>
-      <Suspense fallback={<p className='text-red-500 font-bold text-2xl text-center'>loading Technologies.....</p>}>
-        <Technologies technologiesPromise = {technologiesPromise}/>
+      <Navbar />
+      <Banner />
+      <Suspense
+        fallback={
+          <p className="text-red-500 font-bold text-2xl text-center">
+            loading Technologies.....
+          </p>
+        }
+      >
+        <Technologies technologiesPromise={technologiesPromise} />
       </Suspense>
-      <Footer/>
-      <ToastContainer/>
+      <Footer />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
